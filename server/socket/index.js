da83e58244d1ad1fc11921e8ad41c6e1d5613884
jintobeans <jtjtjtjtw@gmail.com>
2018-03-27 18:21:32 -0700

    comment sockets out

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,15 +1,15 @@
 
-module.exports = io => {
+// module.exports = io => {
 
-  io.on('connection', (socket) => {
-    console.log(`A socket connection to the server has been made: ${socket.id}`)
+//   io.on('connection', (socket) => {
+//     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
-    socket.on('new-message', message => {
-      socket.broadcast.emit('new-message', message);
-    })
+//     socket.on('new-message', message => {
+//       socket.broadcast.emit('new-message', message);
+//     })
 
-    socket.on('disconnect', () => {
-      console.log(`Connection ${socket.id} has left the building`)
-    })
-  })
-};
+//     socket.on('disconnect', () => {
+//       console.log(`Connection ${socket.id} has left the building`)
+//     })
+//   })
+// };
